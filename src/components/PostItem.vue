2f69d07210f2448e6682d398fef5435502528f4a
rusairdrop <rusairdrop@gmail.com>
2021-12-10 13:41:04 +0300

    postitem component added

---
+++ src/components/PostItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  margin-top: 15px;
+  padding: 15px;
+  border: 2px solid teal;
+}
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="post" v-for="post in posts">
-    <div><strong>Название:</strong> {{ post.title }}</div>
-    <div><strong>Описание:</strong> {{ post.body }}</div>
+  <div>
+    <h3>Список постов</h3>
+    <post-item
+        v-for="post in posts"
+        :post="post"
+    />
   </div>
 </template>
 
 <script>
+import PostItem from "./PostItem";
 export default {
+  components: {PostItem},
   props: {
     posts: {
       type: Array,
@@ -17,9 +22,5 @@ export default {
 </script>
 
 <style scoped>
-.post {
-  margin-top: 15px;
-  padding: 15px;
-  border: 2px solid teal;
-}
+
 </style>
