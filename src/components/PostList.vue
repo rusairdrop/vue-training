6f34c2cb23f6f6086e890466f34fbe628f5fda72
rusairdrop <rusairdrop@gmail.com>
2021-12-11 15:44:18 +0300

    amination on post-list addded

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: brown">
     Посты не найдены
@@ -15,6 +17,7 @@
 
 <script>
 import PostItem from "./PostItem";
+
 export default {
   components: {PostItem},
   props: {
@@ -27,5 +30,23 @@ export default {
 </script>
 
 <style scoped>
+.post-list {
+  display: inline-block;
+  margin-right: 10px;
+}
 
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.post-list-move {
+  transition: transform 0.5s ease;
+}
 </style>
